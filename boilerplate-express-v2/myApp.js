393855c105b135a166bdb2edd457e354364586d3
Kevin Thibodeaux <kthib16@gmail.com>
2022-05-26 11:23:30 -0400

    updates env variables

--- boilerplate-express-v2/myApp.js
+++ boilerplate-express-v2/myApp.js
@@ -1,39 +1,26 @@
+
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res) => {
+  let absolutePath = __dirname + '/views/index.html'
+
+  res.sendFile(absolutePath)
+  app.use('/public', express.static(__dirname + '/public'))
 
 
+})
 
+app.get('/json', (req, res) => {
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    let object = {
+      "message": "HELLO JSON"
+    }
+  } else {
+  let object = {
+    "message": "Hello json"
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+  res.json(object)
+})
